0fcee7dc6d8835cdd87a2ee6c4208a43d3cd83e2
jasmeet <harleengulati03@gmail.com>
2022-07-05 15:39:32 +0100

    2nd video watched

--- test.py
+++ test.py
@@ -5,15 +5,31 @@
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 import tensorflow
 import keras
+import numpy as np
+import pandas as pd
+import sklearn
+from sklearn import linear_model
+from sklearn.utils import shuffle
+
+# below reads in all the data
+data = pd.read_csv("student-mat.csv", sep=";")
+
+# select only a few attributes from the 33 available ones
+data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
+
+# the value we want to obtain is known as the "label"
+predict = "G3"
+
+# returns a new data frame without G3 in it (X is the training data)
+# based on training data we predict another value
+X = np.array(data.drop([predict], 1))
+y = np.array(data[predict])
+
+# split 10% of data into test data and 90% is training data
+# otherwise if all was training data the machine learning model would simply memorise the data
+x_train , y_train, x_test, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.1)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print("Hi, {0}".format(name))  # Press ⌘F8 to toggle the breakpoint.
 
+print("Hello!")
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
