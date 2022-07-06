f9a153fe1d65d7eb4dcef92761357014e6d18239
jasmeet <harleengulati03@gmail.com>
2022-07-06 11:41:19 +0100

    3rd video watched

--- test.py
+++ test.py
@@ -1,8 +1,4 @@
-# coding=utf-8
-# This is a sample Python script.
 
-# Press ⇧F10 to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 import tensorflow
 import keras
 import numpy as np
@@ -22,14 +18,32 @@ predict = "G3"
 
 # returns a new data frame without G3 in it (X is the training data)
 # based on training data we predict another value
-X = np.array(data.drop([predict], 1))
+X = np.array(data.drop([predict], axis=1))
 y = np.array(data[predict])
 
 # split 10% of data into test data and 90% is training data
 # otherwise if all was training data the machine learning model would simply memorise the data
-x_train , y_train, x_test, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.1)
+x_train , x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.1)
 
+linear = linear_model.LinearRegression()
 
+# finds a line of best fit from the data we provided in
+linear.fit(x_train, y_train)
+# returns to us a value which represents the accuracy of our model
+acc = linear.score(x_test, y_test)
+print(acc)
+# prints the coefficients of our 5 parameter multidimensional line (coefficient of each attribute)
+# the bigger the coefficient, the more weight each attribute has
+print("Co : \n" , linear.coef_)
+print("Intercept: \n" , linear.intercept_)
+
+# we take in an array of arrays (each array is the attributes we would like to consider)
+# predictions then gives us a corresponding 'guess' for what the final result will be for each of these arrays
+predictions = linear.predict(x_test)
+
+# for comparison between what the model predicts and what the actual value should have been
+for x in range(len(predictions)):
+    print(predictions[x], x_test[x], y_test[x])
 
 print("Hello!")
 
